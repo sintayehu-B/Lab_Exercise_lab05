2e09ec914865a2a76b0b059b2ec279fa7f55ba6d
sintayehu-B <61502710+sintayehu-B@users.noreply.github.com>
2021-01-31 22:19:27 +0300

    bom.js the ul dome

--- TaskManager/assets/js/bom.js
+++ TaskManager/assets/js/bom.js
@@ -18,13 +18,44 @@ Good Luck !!!
 
 
 // Define UI Variables  here 
+const href = document.querySelector('.badge');
+const protocol = document.querySelector('.badge');
+const host = document.querySelector('.badge');
+const port = document.querySelector('.badge');
+const hostName = document.querySelector('.badge');
+
+// browser
+const appName = document.querySelector('.badge');
+const appVersion = document.querySelector('.badge');
+const platForm = document.querySelector('.badge');
+const language = document.querySelector('.badge');
+const cookieEnabled = document.querySelector('.badge');
 
 
+// screen
+
+const height = document.querySelector('.badge');
+const width = document.querySelector('.badge');
+const pixelDepth = document.querySelector('.badge');
+
+// browser history
+const length = document.querySelector('.badge');
+const state = document.querySelector('.badge');
 
 
+const height = document.querySelector('.badge');
+const width = document.querySelector('.badge');
+const pixelDepth = document.querySelector('.badge');
+
+// browser history
+const length = document.querySelector('.badge');
+const state = document.querySelector('.badge');
 
 
 
 
 
 // Display the BOM Information on the innerHTML of the elements
+
+href.innerHTML = window.location.
+// 
